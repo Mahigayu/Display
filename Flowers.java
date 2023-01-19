b07a3dcdcf7db87eb48c9698b81ce94b79bcd419
Mahigayu <123053121+Mahigayu@users.noreply.github.com>
2023-01-19 13:50:51 +0530

    demo for diff

--- Flowers.java
+++ Flowers.java
@@ -1,5 +1,6 @@
 public class Flowers{
 public static void Main(String[] agrs){
+  System.out.println("demo diff tool");
 System.out.println("flowerss");
   System.out.println("changes added in github for pull demo");
 System.out.println("line to remove");
